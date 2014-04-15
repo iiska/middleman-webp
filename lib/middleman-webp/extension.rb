1957fe8c64342ea4e3a67c30cf7c239ac1d4839a
Juhamatti Niemelä <iiska@iki.fi>
2014-04-15 13:36:00 +0300

    Check for external dependencies first during build
    Abort conversion and show error message if missing external dependencies
    ie. command line tools cwebp and gif2webp.
    
    Related to issue #3

--- lib/middleman-webp/extension.rb
+++ lib/middleman-webp/extension.rb
@@ -1,6 +1,8 @@
 require 'middleman-core'
 require 'middleman-webp/converter'
 
+require 'shell'
+
 module Middleman
   class WebPExtension < Extension
     option(:conversion_options, {},
@@ -8,11 +10,24 @@ module Middleman
     option(:ignore, [], 'Ignores files with matching paths')
     def initialize(app, options_hash = {}, &block)
       super
+      @app = app
+    end
 
-      configuration = options
+    def after_build(builder)
+      return unless dependencies_installed?(builder)
+      Middleman::WebP::Converter.new(@app, options, builder).convert
+    end
 
-      app.after_build do |builder|
-        Middleman::WebP::Converter.new(app, configuration, builder).convert
+    # Internal: Check that cwebp and gif2webp commandline tools are available.
+    #
+    # Returns true if all is OK.
+    def dependencies_installed?(builder)
+      sh = Shell.new
+      begin
+        true if sh.find_system_command('cwebp') and sh.find_system_command('gif2webp')
+      rescue Exception => e
+        builder.say_status :webp, "ERROR: #{e.message} Please install cwebp and gif2webp commandline tools first.", :red
+        false
       end
     end
   end
---
+++ spec/unit/extension_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+#require 'pathname'
+#require 'middleman-core'
+require_relative '../../lib/middleman-webp/extension'
+
+describe Middleman::WebPExtension do
+
+  before do
+    #Middleman::Extension.any_instance.expects(:initialize).returns(true)
+    app_mock = stub({
+                      initialized: '',
+                      instance_available: true,
+                      after_configuration: nil,
+                      after_build: nil
+                    })
+    @extension = Middleman::WebPExtension.new(app_mock)
+  end
+
+  describe '#dependencies_installed?' do
+    it 'returns true if required command line tools are found' do
+      Shell.any_instance.expects(:find_system_command).with('cwebp').returns('/usr/bin/cwebp')
+      Shell.any_instance.expects(:find_system_command).with('gif2webp').returns('/usr/bin/gif2webp')
+
+      @extension.dependencies_installed?(stub(say_status: '')).must_equal true
+    end
+
+    it 'returns false and displays error if cwebp is missing' do
+      Shell.any_instance.expects(:find_system_command).with('cwebp').raises(Shell::Error::CommandNotFound)
+      Shell.any_instance.stubs(:find_system_command).with('gif2webp').returns('/usr/bin/gif2webp')
+
+      builder_mock = stub(:say_status)
+      builder_mock.stubs(:say_status).once
+
+      @extension.dependencies_installed?(builder_mock).must_equal false
+    end
+
+    it 'returns false and displays error if gif2webp is missing' do
+      Shell.any_instance.expects(:find_system_command).with('gif2webp').raises(Shell::Error::CommandNotFound)
+      Shell.any_instance.stubs(:find_system_command).with('cwebp').returns('/usr/bin/cwebp')
+
+      builder_mock = stub(:say_status)
+      builder_mock.stubs(:say_status).once
+
+      @extension.dependencies_installed?(builder_mock).must_equal false
+    end
+  end
+
+end
