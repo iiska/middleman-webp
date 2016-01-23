851468f5f78ab461db3b78d1cc93b8881c0253a8
Juhamatti Niemelä <iiska@iki.fi>
2016-01-23 13:30:58 +0200

    Fix Rubocop issues at WebPExtension class

--- lib/middleman-webp/extension.rb
+++ lib/middleman-webp/extension.rb
@@ -4,16 +4,24 @@ require 'middleman-webp/converter'
 require 'shell'
 
 module Middleman
+  ##
+  # Middleman extension for converting image assets to WebP alternatives
+  #
+  # Conversion is run using after_build or before_build according to the
+  # configuration, after_build being the default.
   class WebPExtension < Extension
     option(:conversion_options, {},
            'Custom conversion options for cwebp/gif2webp')
     option(:append_extension, false,
-           'keep the original filename and extension and append .webp (image.png => image.png.webp)')
+           'keep the original filename and extension and append .webp '\
+           '(image.png => image.png.webp)')
     option(:ignore, [], 'Ignores files with matching paths')
     option(:verbose, false, 'Display all external command which are executed '\
            'to help debugging.')
-    option(:allow_skip, true, 'Skip saving .webp files which are larger than their source')
-    option(:run_before_build, false, 'Run before build and save .webp files in source dir')
+    option(:allow_skip, true, 'Skip saving .webp files which are larger than '\
+           'their source')
+    option(:run_before_build, false, 'Run before build and save .webp files in'\
+           ' source dir')
 
     def initialize(app, options_hash = {}, &block)
       super
@@ -48,15 +56,15 @@ module Middleman
       Shell.new.find_system_command('gif2webp')
     rescue Shell::Error::CommandNotFound => e
       builder.trigger :webp, nil, "#{e.message} Please install latest version"\
-        " of webp library and tools to get gif2webp and be able to convert gif"\
-        "files also."
+        ' of webp library and tools to get gif2webp and be able to convert gif'\
+        'files also.'
     end
 
     def cwebp_installed?(builder)
       true if Shell.new.find_system_command('cwebp')
     rescue Shell::Error::CommandNotFound => e
       builder.trigger :error, "ERROR: #{e.message} Please install cwebp and "\
-        "gif2webp commandline tools first."
+        'gif2webp commandline tools first.'
       false
     end
   end
